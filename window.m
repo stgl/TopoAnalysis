23764819b1486607dd9b55281153befdf5db252f
rmsare <robertmsare@gmail.com>
2016-02-16 18:35:05 -0800

    update docstring

--- window.m
+++ window.m
@@ -1,10 +1,18 @@
 function outgrid = window(ingrid, radius, functionname)
    
-    %% Compute moving average of parameter on grid using window radius
+    %% Compute arbitrary function on grid using a square kernel of radius
     %% specified in grid resolution units
+    %%
+    %% INPUT:   ingrid - input grid struct
+    %%          radius - search radius in grid resolution units
+    %%          functionname - function handle for desired calculation
+    %%          takes two arguments: x, the central value
+    %%                               v, column vector of neighboring values
+    %%
+    %% EXAMPLE: local relief over 100 m radius search kernel 
+    %% lrelief = @(x, v) nanmax([x v']) - nanmin([x v']);
+    %% outgrid = window(dem, 100, lrelief);
     %% 
-    %% Input function takes central value as first argument and vector of values
-    %% in search kernel as subsequent arguments
 
     [m,n] = size(ingrid.grid);
     outgrid = ingrid;
