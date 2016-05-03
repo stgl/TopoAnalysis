2f14ecb125a5f073ed975f3826a1b89058e349d9
George Hilley <gehilley@gmail.com>
2016-05-03 12:36:37 -0700

    050316: GEH: Changed back to zip

--- demMethods.py
+++ demMethods.py
@@ -1,16 +1,16 @@
 def read(filename):
-    import gzip
+    import bz2
     import cPickle as pickle
-    f = gzip.GzipFile(filename + '.gz', 'rb') 
+    f = bz2.BZ2File(filename + '.gz', 'rb') 
     obj = pickle.load(f)
     f.close()
     return obj
 
 def write(obj, filename):
     
-    import gzip
+    import bz2
     import cPickle as pickle
-    f = gzip.GzipFile(filename + '.gz', 'wb')
+    f = bz2.BZ2File(filename + '.gz', 'wb')
     s = pickle.dumps(obj)
     for i in range(0, len(s), 2**32):
         f.write(bytes(s[i:i+2**32]))
