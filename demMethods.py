a5ff0f31095bb6df15c4a91d203cd3350cebc25a
George Hilley <gehilley@gmail.com>
2016-05-09 11:20:47 -0700

    050916: GEH: final modifications

--- demMethods.py
+++ demMethods.py
@@ -7,20 +7,20 @@ def processAll(prefix_name, Ao, theta, base_name = '.'):
     d8_name = base_name + "/" + prefix_name + "_dir_15s"
     
     elevation = Elevation(gdal_filename = elevation_name)
+    elevation.save(prefix_name + "_elevation")
     area = Area(gdal_filename = area_name)
+    area.save(prefix_name + "_area")
     d8 = FlowDirectionD8(gdal_filename = d8_name)
+    d8.save(prefix_name + "_flow_direction")
     
     idx = area.sort(reverse = False)
     area = GeographicArea(flow_direction = d8, sorted_indexes = idx)
-    flow_length = GeographicFlowLength(flow_direction = d8, sorted_indexes = idx)
-    ksi = GeographicKsi(area = area, flow_direction = d8, theta = theta, Ao = Ao, flow_length = flow_length, sorted_indexes = idx)
-    relief = ScaledRelief(flow_direction = d8, elevation = elevation, flow_length = flow_length, Ao = Ao, theta = theta, sorted_indexes = idx)
-    
-    elevation.save(prefix_name + "_elevation")
-    d8.save(prefix_name + "_flow_direction")
     area.save(prefix_name + "_area")
-    ksi.save(prefix_name + "_ksi_" + str(Ao).replace('.','_') + "_" + str(theta).replace('.','_'))
+    flow_length = GeographicFlowLength(flow_direction = d8, sorted_indexes = idx)
     flow_length.save(prefix_name + "_flow_length")
+    ksi = GeographicKsi(area = area, flow_direction = d8, theta = theta, Ao = Ao, flow_length = flow_length, sorted_indexes = idx)
+    ksi.save(prefix_name + "_ksi_" + str(Ao).replace('.','_') + "_" + str(theta).replace('.','_'))
+    relief = ScaledRelief(flow_direction = d8, elevation = elevation, flow_length = flow_length, Ao = Ao, theta = theta, sorted_indexes = idx)
     relief.save(prefix_name + "_relief_" + str(Ao).replace('.','_') + "_" + str(theta).replace('.','_'))
     
 def processForTheta(prefix_name, Ao, theta, base_name = '.'):
@@ -39,8 +39,7 @@ def processForTheta(prefix_name, Ao, theta, base_name = '.'):
     
     idx = area.sort(reverse = False)
     ksi = GeographicKsi(area = area, flow_direction = d8, theta = theta, Ao = Ao, flow_length = flow_length, sorted_indexes = idx)
-    relief = ScaledRelief(flow_direction = d8, elevation = elevation, flow_length = flow_length, Ao = Ao, theta = theta, sorted_indexes = idx)
-    
     ksi.save(prefix_name + "_ksi_" + str(Ao).replace('.','_') + "_" + str(theta).replace('.','_'))
+    relief = ScaledRelief(flow_direction = d8, elevation = elevation, flow_length = flow_length, Ao = Ao, theta = theta, sorted_indexes = idx)
     relief.save(prefix_name + "_relief_" + str(Ao).replace('.','_') + "_" + str(theta).replace('.','_'))
     
