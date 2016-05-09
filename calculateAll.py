5489766aaa7adc6424b488f43d3794f7f933627a
George Hilley <gehilley@gmail.com>
2016-05-09 14:11:47 -0700

    050916: GEH: bug

--- calculateAll.py
+++ calculateAll.py
@@ -1,7 +1,7 @@
 import argparse, sys, os
 
 parser = argparse.ArgumentParser(description='Calculate grids for ksi analysis for hydrosheds 500 m data')
-parser.add_argument('-b','--base-directory', metavar = 'b', help='base directory containing input files.')
+parser.add_argument('-b','--base_directory', help='base directory containing input files.')
 parser.add_argument('prefix', metavar='dir', type=str, 
                    help='prefix of hydrosheds dataset directory')
 parser.add_argument('Ao', metavar='Ao', type=float, 
@@ -12,7 +12,7 @@ parser.add_argument('theta', metavar='theta', type=float,
 args = parser.parse_args()
 
 from demMethods import processAll
-if args.b:
+if args.base_directory:
     processAll(args.prefix, args.Ao, args.theta, args.b)
 else:
     processAll(args.prefix, args.Ao, args.theta)
