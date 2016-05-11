6a4b8c9dd7ae19fbb22e1b8308b0e9cc7fdea1fc
George Hilley <gehilley@gmail.com>
2016-05-11 11:52:19 -0700

    051116: GEH: Reworked relief calculation

--- dem.py
+++ dem.py
@@ -1277,15 +1277,14 @@ class Relief(BaseSpatialGrid, MaxFlowLengthTrackingMixin):
     
     def _create_from_flow_direction_sorted_indexes_and_elevation(self, *args, **kwargs):
         self._copy_info_from_grid(kwargs['flow_direction'], True)
+        self._griddata = kwargs['elevation']._griddata
         self._calculate_by_tracking_down_max_flow_length(*args, **kwargs)
+        self._griddata = kwargs['elevation']._griddata - self._griddata
     
     def _calculate_grid_value(self, pos, next_pos, *args, **kwargs):
         (i,j) = pos
         (i_next, j_next) = next_pos
-        drf = kwargs['elevation'][i,j] - kwargs['elevation'][i_next, j_next]
-        if drf < 0:
-            drf = 0
-        self._griddata[i_next, j_next] = self._griddata[i,j] + drf
+        self._griddata[i_next, j_next] = self._griddata[i,j]
         
 class ScaledRelief(Relief):
     
