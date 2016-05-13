d758fc2b0ae86b736b90b21e0ee8e24bf47b71d1
George Hilley <gehilley@gmail.com>
2016-05-13 11:27:30 -0700

    051316: GEH: eliminated asia from processing file

--- calculateAllThetaCombinations.py
+++ calculateAllThetaCombinations.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
+import demMethods as d
+
 Ao = 250000
 theta = 0.4
 
-import demMethods as d
-
 d.processForTheta('af', Ao, theta)
-d.processForTheta('as', Ao, theta)
 d.processForTheta('au', Ao, theta)
 d.processForTheta('ca', Ao, theta)
 d.processForTheta('eu', Ao, theta)
@@ -16,13 +15,9 @@ d.processForTheta('sa', Ao, theta)
 theta = 0.6
 
 d.processForTheta('af', Ao, theta)
-d.processForTheta('as', Ao, theta)
 d.processForTheta('au', Ao, theta)
 d.processForTheta('ca', Ao, theta)
 d.processForTheta('eu', Ao, theta)
 d.processForTheta('na', Ao, theta)
 d.processForTheta('sa', Ao, theta)
 
-theta = 0.5
-
-d.processForTheta('as', Ao, theta)
