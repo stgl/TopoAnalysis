3158c2fd6bbf2902e85c95aac7b8a9c9c8995acf
George Hilley <gehilley@gmail.com>
2016-05-16 15:00:16 -0700

    501616: GEH: all figure modification

--- plotGlobalKsn_theta_0_5.py
+++ plotGlobalKsn_theta_0_5.py
@@ -14,8 +14,8 @@ for prefix in prefixes:
     
     fig = plt.figure()
     ksi_vec, relief_vec = plotGrids(ksi, relief, 'k.', rasterized = True)
-    plt.savefig(prefix + '0_5.png',dpi=600)
-    plt.close()
+    #plt.savefig(prefix + '0_5.png',dpi=600)
+    #plt.close()
     
     if 'all_ksi_vec' not in locals():
         all_ksi_vec = ksi_vec
@@ -23,10 +23,10 @@ for prefix in prefixes:
     else:
         all_ksi_vec = np.append(all_ksi_vec,ksi_vec)
         all_relief_vec = np.append(all_relief_vec, relief_vec)
+    print('loaded ' + prefix)
         
-    
 fig = plt.figure()
 
-plt.plot(all_ksi_vec, all_relief_vec, 'k.', rasterize = True)
+plt.plot(all_ksi_vec, all_relief_vec, 'k.', rasterized = True)
 plt.savefig('all_0_5.png',dpi=600)
     
