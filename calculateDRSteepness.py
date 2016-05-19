25ab8b1637d15cfcab0f50d504667e4539a6863d
George Hilley <gehilley@gmail.com>
2016-05-19 10:37:37 -0700

    501916: GEH: added save to unscript, changed permissions

--- calculateDRSteepness.py
+++ calculateDRSteepness.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python2.7
 
 import denudationRateAnalysis as dra
+import numpy as np
+
 data = dra.read_csv('portengadata.csv')
 del(data[0])
 ksn_vec, area_vec = dra.calculate_ksn_for_data(data)
+
+np.savez_compressed('ksn_area_data.npz', ksn_vec = ksn_vec, area_vec = area_vec)
