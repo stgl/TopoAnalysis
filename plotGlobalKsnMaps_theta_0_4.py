11a2c27bde372b374b55ce769f2333f731a7c73f
George Hilley <gehilley@gmail.com>
2016-10-04 16:40:16 -0700

    100416: GEH: Made script for creating maps

---
+++ plotGlobalKsnMaps_theta_0_4.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python2.7
+
+prefixes = ('af', 'as', 'au', 'ca', 'eu', 'na', 'sa')
+threshold = 100
+
+import matplotlib
+matplotlib.use('Agg')
+
+import dem as d
+
+a = [0, 35000, 0, 1200000]
+suffix = '0_4'
+
+for prefix in prefixes:
+    
+    ksi = d.Ksi.load(prefix + '_ksi_250000_' + suffix)
+    relief = d.ScaledRelief.load(prefix + '_relief_250000_' + suffix)
+    ks = ((relief._griddata / ksi._griddata) >= threshold).astype(int)
+    
+    ks.save(prefix + '_ks_mask_250000_' + suffix + "_" + threshold)
+        
+    
