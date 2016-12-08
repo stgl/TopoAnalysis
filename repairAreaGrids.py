fa8021c98f4b646753e6969346a1af900affa3bd
George Hilley <gehilley@gmail.com>
2016-12-07 17:36:49 -0800

    120716: GEH: Modifications to fix area problem.

--- repairAreaGrids.py
+++ repairAreaGrids.py
@@ -1,11 +1,15 @@
 import dem as d
+base_path = '/data/sesfs/scarp1/GlobalSteepness/Datasets/Grids/'
 
 prefixes = ['af', 'as', 'au', 'ca', 'eu', 'na', 'sa']
 
 for prefix in prefixes:
-    area = d.GeographicArea.load(prefix + '_area')
+    area = d.GeographicArea(gdal_filename = base_path + prefix + '/' + prefix + '_acc_15s')
+    print('Successfully read: ' + prefix)
     d8 = d.FlowDirectionD8.load(prefix + '_flow_direction')
     idx = area.sort(reverse = False)
+    print('Sorting done.')
     area = d.GeographicArea(flow_direction = d8, sorted_indexes = idx)
+    print('Area recomputed.')
     area.save(prefix + "_area")
-    
+    print('File saved.')
