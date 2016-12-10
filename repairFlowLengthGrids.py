bda1f35d18feb0d3688e17eaa546cf6c6cc25a73
George Hilley <hilley@stanford.edu>
2016-12-10 13:24:04 -0800

    121016: GEH: script to repair flow length grids

---
+++ repairFlowLengthGrids.py
@@ -0,0 +1,16 @@
+import dem as d
+from builtins import None
+
+prefixes = ['af', 'as', 'au', 'ca', 'eu', 'na', 'sa']
+
+for prefix in prefixes:
+    
+    print('Started ' + prefix)
+    area = d.GeographicArea.load(prefix + '_area')
+    idx = area.sort(reverse = False)
+    area = None
+    d8 = d.FlowDirectionD8.load(prefix + '_flow_direction')
+    flow_length = d.GeographicFlowLength(flow_direction = d8, sorted_indexes = idx)
+    flow_length.save(prefix + '_flow_length')
+    print('Done with ' + prefix)
+    
