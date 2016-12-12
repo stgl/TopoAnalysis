61a9704d9f9bbd65dc4d2fe1289ea5a125744268
George Hilley <hilley@stanford.edu>
2016-12-12 06:55:51 -0800

    121216: GEH: Scripts to calculate chi in background

---
+++ runChi_as.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python2.7
+
+from denudationRateAnalysis import create_chi_grid_for_geographic_prefix as create_chi
+
+prefix = 'as' 
+
+thetas = [0.4, 0.5, 0.6]
+Ao = 1000000
+basin_lengths = [50000, 100000, 200000, 400000]
+
+create_chi(prefix, thetas, Ao, basin_lengths)
+
