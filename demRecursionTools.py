bf70a7ace00dc3336a91be6629d16a805ed25f4d
George Hilley <gehilley@gmail.com>
2017-10-12 10:48:53 -0700

    101217: GEH: debugged recursion tools

--- demRecursionTools.py
+++ demRecursionTools.py
@@ -4,10 +4,10 @@ def extract_chi_elevation_values(ld_list, de, theta, chi_o, elevation, chi, base
 
     Ao = np.power(xo, 2.0)
     if ld_list['area'] >= Ao:
-        elevation_f = ld_list['elevation'] - base_elevation
-        elevation = elevation + [elevation_f]
+        elevation_f = np.array(ld_list['elevation'] - base_elevation)
+        elevation = np.append(elevation, np.array(elevation_f))
         index = ld_list['index']
-        chi_f = chi_o + (1 / ld_list['area'])**theta * ld_list['distance_scale'] * de[index[0], index[1]]
+        chi_f = chi_o + (1 / np.array(ld_list['area']))**theta[0] * np.array(ld_list['distance_scale']) * de[index[0], index[1]]
         chi = chi + [chi_f]
         if ld_list.get('next') is not None:
             for next_list in ld_list['next']:
@@ -21,8 +21,8 @@ def chi_elevation(ld_list, de, theta, xo = 500.0):
     elevation = []
     chi = []
     base_elevation = ld_list['elevation']
-    
-    return extract_chi_elevation_values(ld_list, de, theta, chi_o, elevation, chi, base_elevation, xo)
+    e, c = extract_chi_elevation_values(ld_list, de, theta, chi_o, elevation, chi, base_elevation, xo)
+    return np.array(e), np.array(c)
 
 def best_ks_with_wrss_list(ld_list, de, theta, xo = 500):
     
@@ -36,7 +36,7 @@ def best_ks_with_wrss_list(ld_list, de, theta, xo = 500):
 
 def uninformative_SS_list(ld_list, de, xo = 500):
     
-    e, c = chi_elevation(ld_list, de, 0.5, xo)
+    e, c = chi_elevation(ld_list, de, [0.5], xo)
     mean_elevation = np.mean(e)
     return np.sum(np.power(e-mean_elevation, 2))
 
@@ -47,10 +47,10 @@ def best_ks_and_theta_with_wrss(elevation, flow_direction, area, outlet, xo = 50
     
     import scipy.optimize
     chi_ks = lambda theta: best_ks_with_wrss_list(ld_list, de, theta, xo)[1]
-    if len(chi_ks(0.5)) == 0:
+    if len(chi_ks([0.5])) == 0:
         return (0, 0, 0)
     xopt = scipy.optimize.fmin(func=chi_ks, x0=np.array([0.5]))
-    (m, WRSS) = best_ks_with_wrss_list(ld_list, de, xopt[0], xo)
+    (m, WRSS) = best_ks_with_wrss_list(ld_list, de, [xopt[0]], xo)
     SS = uninformative_SS_list(ld_list, de, xo)
     
     return (m, xopt[0], 1 - (WRSS / SS))
