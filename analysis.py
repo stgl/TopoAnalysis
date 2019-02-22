846ec28b9328facbadbcf37732dceece5f167665
rmsare <robertmsare@gmail.com>
2019-02-22 14:48:59 -0800

    Formatting per flake8

--- analysis.py
+++ analysis.py
@@ -1,5 +1,3 @@
-import os
-
 import numpy as np
 import rasterio
 
@@ -7,11 +5,8 @@ import matplotlib.pyplot as plt
 
 from itertools import product
 
-from dem import BaseSpatialGrid, ScarpWavelet
-
 
 class Quadrats(object):
-
 
     def __init__(self, filename=None, data=None, band=1, dx=None, dy=None):
         if filename is not None:
@@ -20,17 +15,15 @@ class Quadrats(object):
             self.data = data
         else:
             self.data = None
-        
+
         if dx is not None:
             self.make_quadrats(dx, dy)
         else:
             self.quadrats = []
 
-
     def load_data(self, filename, band=1):
         with rasterio.open(filename, 'r') as src:
             self.data = src.read(band)
-
 
     def make_quadrats(self, dx, dy=None):
         if dy is None:
@@ -38,18 +31,16 @@ class Quadrats(object):
         ny, nx = self.data.shape
         rows = np.arange(0, ny-dy+1, step=dy)
         cols = np.arange(0, nx-dx+1, step=dx)
-        
-        #rows += row_offset
-        #cols += col_offset
-        #rows = rows[rows <= ny]
-        #cols = cols[cols <= nx]
+
+        # rows += row_offset
+        # cols += col_offset
+        # rows = rows[rows <= ny]
+        # cols = cols[cols <= nx]
 
         self.quadrats = [self.data[r:r+dy, c:c+dx] for r in rows for c in cols]
 
-
     def map_quadrats(self, func, **kwargs):
         return [func(q, **kwargs) for q in self.quadrats]
-
 
     def plot(self, values, ax=None, **kwargs):
         ny, nx = self.data.shape
@@ -67,7 +58,6 @@ class Quadrats(object):
         ax.set_xlim([0, nx])
         ax.set_ylim([0, ny])
         ax.invert_yaxis()
-
 
     def quiver(self, u, v, ax=None, **kwargs):
         ny, nx = self.data.shape
